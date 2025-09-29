814599240463523af028253d9be712dcdf1935e2
Subash Shanmugam <subashs2573@gmail.com>
2025-09-29 12:50:52 +0530

    Some Features are added

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,17 +3,19 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
+    // Temporarily disabled until Firebase is properly configured
+    // id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.alphacreators.hcsub.hcsub"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -46,5 +48,6 @@ flutter {
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
